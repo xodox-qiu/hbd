99177098bad94e7903775cb65aace9ddf3362e6b
dvalinnsimulacrum <dvalinnsimulacrum@gmail.com>
2024-10-16 16:18:43 +0700

    new-3 => done

--- app/src/main/java/diddy/example/happybirthday/Kotl-Base.kt
+++ app/src/main/java/diddy/example/happybirthday/Kotl-Base.kt
@@ -63,7 +63,101 @@ fun printFinalTemperature(
     println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }*/
 
+/* Popular Song
+class PopSong(
+    val title: String,
+    val artist: String,
+    val year: Int,
+    var playCount: Int
+) {
+    val isPopular: Boolean
+        get() = playCount >= 1000
+    fun printDescription() {
+        println("$title, dibawakan oleh $artist, dirilis pada tahun $year.")
+    }
+}
+fun main() {
+    val song = PopSong("Cancer", "My Chemical Romance", 2007, 237_000_000)
+    song.printDescription()
+    println("${song.title} popular? ${song.isPopular}")
+}*/
 
+/* Internet Profile
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        println("Likes to $hobby.")
+        if(referrer != null) {
+            print("Has a referrer named ${referrer.name}")
+        	if(referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}.")
+            } else {
+                print(".")
+            }
+        } else {
+            print("Doesn't have a referrer.\n\n")
+        }
+    }
+}*/
+
+/* Foldable Phone
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isFolded: Boolean = true): Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+    fun fold() {
+        isFolded = true
+    }
+    fun unfold() {
+        isFolded = false
+    }
+}
+fun main() {
+    val newFoldablePhone = FoldablePhone()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+}*/
+
+/* Lelang
+fun main() {
+    val winningBid = Bid(5000, "Private Collector")
+
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
+}
+
+class Bid(val amount: Int, val bidder: String)
+
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+    return bid?.amount ?: minimumPrice
+}*/
 
 
 
